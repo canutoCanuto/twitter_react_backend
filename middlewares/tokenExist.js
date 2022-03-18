50b3669ea1cac69893d65c81327089d1508933d8
PabloSpr <sw.pablo@gmail.com>
2022-03-18 10:42:54 -0300

    middleware --> try-catch

--- middlewares/tokenExist.js
+++ middlewares/tokenExist.js
@@ -1,10 +1,14 @@
 const User = require("../models/User");
 
 const tokenExist = async (req, res, next) => {
-  const { tokens } = await User.findById(req.user.sub, { tokens: 1 });
-  const [Bearer, actualToken] = req.headers.authorization.split(" ");
-  const exist = tokens.some((token) => token === actualToken);
-  if (exist) return next();
-  res.json({ message: "error" });
+  try {
+    const { tokens } = await User.findById(req.user.sub, { tokens: 1 });
+    const [Bearer, actualToken] = req.headers.authorization.split(" ");
+    const exist = tokens.some((token) => token === actualToken);
+    if (exist) return next();
+    res.json({ message: "error" });
+  } catch (error) {
+    res.json({ message: error });
+  }
 };
 module.exports = tokenExist;
